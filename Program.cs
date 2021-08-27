f3eff11b5ac2b68f009dc449c2c814f739fc3a92
Elenilson Silva <elenilsonsilva1996si@gmail.com>
2021-08-27 15:46:14 +0000

    metodo converterConta do projeto da conta bancaria finalizado

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new ContaCorrente(20);
+            var contaI = new ContaInvestimento(30);
+
+            var contaNova  = ConverteConta<ContaCorrente,ContaInvestimento>(conta);
+            Console.WriteLine(contaNova.Saldo);
+            
+        }
+
+        public static G ConverteConta<T, G>(T contaAtual) where T : Conta where G : Conta
+        {
+            G contaConvertida = Activator.CreateInstance<G>();
+           
+            contaConvertida.Saldo = contaAtual.Saldo;
+            contaConvertida.Numero = contaAtual.Numero;
+            contaConvertida.Agencia = contaAtual.Agencia;
+
+            return contaConvertida;
+        }
+    }
+}
